8b422642d213feb12ef409546a58d050de20951c
Sam Rice <samsrice@gmail.com>
2022-08-24 17:04:44 -0700

    Initial commit

--- src/book.js
+++ src/book.js
@@ -1,11 +1,33 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return "The " + title;
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  };
+};
+
+function saveReview(newReview, reviews) {
+  if (reviews.indexOf(newReview) === -1) {
+    reviews.push(newReview);
+  }
 }
+
+
+//   if (newReview !== reviews) {
+//   reviews.push(newReview);
+//   }
+// };
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
