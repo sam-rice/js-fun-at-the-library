a82fc06f4b31021d5b0db1c0a13afd0ac4cfefd0
Sam Rice <samsrice@gmail.com>
2022-08-25 20:23:44 -0700

    complete iteration 1

--- src/book.js
+++ src/book.js
@@ -13,22 +13,32 @@ function buildMainCharacter(name, age, pronouns) {
 function saveReview(newReview, reviews) {
   if (reviews.indexOf(newReview) === -1) {
     reviews.push(newReview);
-  }
-}
+  };
+};
 
+function calculatePageCount(title) {
+  return title.length * 20;
+};
 
-//   if (newReview !== reviews) {
-//   reviews.push(newReview);
-//   }
-// };
+function writeBook(bookTitle, bookCharacter, genre) {
+  return {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre
+  };
+};
 
+function editBook(book) {
+  return book.pageCount = (book.pageCount / 4) * 3;
+};
 
 
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
-}
+  calculatePageCount,
+  writeBook,
+  editBook
+};
