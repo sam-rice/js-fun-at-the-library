dce2561d9e2e6ece7ec8ff2eb41cce5ada4d1537
Sam Rice <samsrice@gmail.com>
2022-08-26 22:46:39 -0700

    add extension iteration

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,38 @@
 class Librarian {
-  constructor() {
-    
-  }
-}
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  };
+
+  greetPatron(name, morning) {
+    if (morning === true) {
+      return `Good morning, ${name}!`
+    } else {
+      return `Hello, ${name}!`
+    };
+  };
+
+  findBook(title) {
+   for (var i = 0; i < this.library.shelves.fantasy.length; i++) {
+    if (this.library.shelves.fantasy[i].title === title) {
+      this.library.shelves.fantasy.splice(i, 1);
+      return `Yes, we have ${title}`;
+    };
+  };
+    return `Sorry, we do not have ${title}`
+  };
+
+  calculateLateFee(days) {
+    return Math.ceil(days * .25);
+  };
+};
+
+function addBook(library, book) {
+  title: book
+};
 
 module.exports = Librarian;
+  {
+  addBook
+  };
+
